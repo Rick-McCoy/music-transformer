Fixed accuracy calculation

model/model.py
@@ -48,7 +48,8 @@ class MusicModel(LightningModule):
         pitch_acc = self.acc(pitch_out, pitch[:, 1:])
         program_acc = self.acc(program_out, program[:, 1:])
         velocity_acc = self.acc(velocity_out, velocity[:, 1:])
-        acc = torch.mean(tick_acc + pitch_acc + program_acc + velocity_acc)
+        acc = torch.mean(
+            torch.stack([tick_acc, pitch_acc, program_acc, velocity_acc]))
         self.log("train_tick_loss", tick_loss)
         self.log("train_tick_acc", tick_acc)
         self.log("train_pitch_loss", pitch_loss)
@@ -61,8 +62,8 @@ class MusicModel(LightningModule):
         self.log("train_acc", acc)
         return loss
 
-    def validation_step(self, batch: Tuple[Tensor, Tensor], *args,
-                        **kwargs) -> Tensor:
+    def validation_step(self, batch: Tuple[Tensor, Tensor, Tensor, Tensor],
+                        *args, **kwargs) -> Tensor:
         tick, pitch, program, velocity = batch
         tick_out, pitch_out, program_out, velocity_out = self.transformer(
             tick[:, :-1], pitch[:, :-1], program[:, :-1], velocity[:, :-1])
@@ -75,7 +76,8 @@ class MusicModel(LightningModule):
         pitch_acc = self.acc(pitch_out, pitch[:, 1:])
         program_acc = self.acc(program_out, program[:, 1:])
         velocity_acc = self.acc(velocity_out, velocity[:, 1:])
-        acc = torch.mean(tick_acc + pitch_acc + program_acc + velocity_acc)
+        acc = torch.mean(
+            torch.stack([tick_acc, pitch_acc, program_acc, velocity_acc]))
         self.log("val_tick_loss", tick_loss)
         self.log("val_tick_acc", tick_acc)
         self.log("val_pitch_loss", pitch_loss)
@@ -126,7 +128,7 @@ class MusicModel(LightningModule):
                                                velocity_preds,
                                                self.current_epoch)
 
-    def test_step(self, batch: Tuple[Tensor, Tensor], *args,
+    def test_step(self, batch: Tuple[Tensor, Tensor, Tensor, Tensor], *args,
                   **kwargs) -> Tensor:
         tick, pitch, program, velocity = batch
         tick_out, pitch_out, program_out, velocity_out = self.transformer(
@@ -140,7 +142,8 @@ class MusicModel(LightningModule):
         pitch_acc = self.acc(pitch_out, pitch[:, 1:])
         program_acc = self.acc(program_out, program[:, 1:])
         velocity_acc = self.acc(velocity_out, velocity[:, 1:])
-        acc = torch.mean(tick_acc + pitch_acc + program_acc + velocity_acc)
+        acc = torch.mean(
+            torch.stack([tick_acc, pitch_acc, program_acc, velocity_acc]))
         self.log("test_tick_loss", tick_loss)
         self.log("test_tick_acc", tick_acc)
         self.log("test_pitch_loss", pitch_loss)