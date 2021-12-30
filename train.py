5f7f09705fc49c2925489e96222ec60b74cac712
Rick-McCoy <rickmccoy3141@gmail.com>
2021-12-31 00:48:07 +0900

    Add num_layers & segments

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import argparse
+import math
 
-import wandb
 from hydra import initialize, compose
+import wandb
 
 from main import main
 
@@ -11,12 +12,15 @@ if __name__ == "__main__":
     wandb.init(config=args)
     config = wandb.config
     with initialize(config_path="config"):
+        segments = round(math.sqrt(config.num_layers))
         cfg = compose(config_name="config",
                       overrides=[
                           f"model.d_model={config.d_model}",
                           f"model.data_len={config.data_len}",
                           f"model.dropout={config.dropout}",
                           f"model.ff={config.ff}",
-                          f"model.nhead={config.nhead}"
+                          f"model.nhead={config.nhead}",
+                          f"model.num_layers={config.num_layers}",
+                          f"model.segments={segments}"
                       ])
         main(cfg)
