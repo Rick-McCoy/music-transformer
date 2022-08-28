88065851e62dac00dd1003fd195f76402399eff5
Rick-McCoy <rickmccoy3141@gmail.com>
2022-08-28 17:25:24 +0900

    Changed repeated code into templates.

--- model/model.py
+++ model/model.py
@@ -54,109 +54,69 @@ class MusicModel(LightningModule):
         self.example_input_array = torch.zeros(1, data_len, dtype=torch.int64)
         self.class_names = ["Special", "Program", "Drum", "Note", "Tick"]
 
-    def forward(self, data: Tensor) -> Tensor:  # pylint: disable=arguments-differ
+    def forward(self, *args, **kwargs) -> Tensor:
+        data = args[0]
         return self.transformer(data)
 
-    def training_step(self, *args, **kwargs) -> Tensor:  # pylint: disable=unused-argument
-        batch = args[0]
+    def step_template(self, batch: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
         output = self(batch[:, :-1])
         loss = self.loss(output, batch[:, 1:])
         acc = self.acc(output, batch[:, 1:])
         self.log("train/loss", loss)
         self.log("train/acc", acc)
+        return loss, output, batch[:, 1:]
+
+    def training_step(self, *args, **kwargs) -> Tensor:
+        loss, *_ = self.step_template(*args, **kwargs)
         return loss
 
-    def validation_step(
-        self, *args, **kwargs
-    ) -> Tuple[Tensor, Tensor, Tensor]:  # pylint: disable=unused-argument
-        batch = args[0]
-        output = self(batch[:, :-1])
-        loss = self.loss(output, batch[:, 1:])
-        acc = self.acc(output, batch[:, 1:])
-        self.log("val/loss", loss)
-        self.log("val/acc", acc)
-        return loss, output, batch[:, 1:]
+    def validation_step(self, *args, **kwargs) -> Tuple[Tensor, Tensor, Tensor]:
+        return self.step_template(*args, **kwargs)
+
+    def test_step(self, *args, **kwargs) -> Tuple[Tensor, Tensor, Tensor]:
+        return self.step_template(*args, **kwargs)
+
+    def epoch_end_template(self, outputs: List[Tuple[Tensor, Tensor, Tensor]], mode: str) -> None:
+        _, output_list, target_list = zip(*outputs)
+        score = torch.cat(output_list, dim=0).permute([0, 2, 1]).flatten(0, 1)
+        target = torch.cat(target_list, dim=0).flatten()
+        y_true = self.simplify_class(target).detach().cpu().numpy()
+        y_probas = self.simplify_score(score).detach().cpu().numpy()
+        assert mode in ["val", "test"], f"Unknown mode {mode}"
+        if self.logger is not None:
+            wandb.log(
+                {
+                    f"{mode}/pr_curve": wandb_plot.pr_curve(
+                        y_true=y_true[:10000],
+                        y_probas=y_probas[:10000],
+                        labels=self.class_names,
+                    )
+                }
+            )
+            wandb.log(
+                {
+                    f"{mode}/roc_curve": wandb_plot.roc_curve(
+                        y_true=y_true[:10000],
+                        y_probas=y_probas[:10000],
+                        labels=self.class_names,
+                    )
+                }
+            )
+            wandb.log(
+                {
+                    f"{mode}/conf_mat": wandb_plot.confusion_matrix(
+                        probs=y_probas,
+                        y_true=y_true,
+                        class_names=self.class_names,
+                    )
+                }
+            )
 
     def validation_epoch_end(self, outputs: List[Tuple[Tensor, Tensor, Tensor]]) -> None:
-        _, output_list, target_list = zip(*outputs)
-        score = torch.cat(output_list, dim=0).permute([0, 2, 1]).flatten(0, 1)
-        target = torch.cat(target_list, dim=0).flatten()
-        y_true = self.simplify_class(target).detach().cpu().numpy()
-        y_probas = self.simplify_score(score).detach().cpu().numpy()
-        if self.logger is not None:
-            wandb.log(
-                {
-                    "val/pr_curve": wandb_plot.pr_curve(
-                        y_true=y_true[:10000],
-                        y_probas=y_probas[:10000],
-                        labels=self.class_names,
-                    )
-                }
-            )
-            wandb.log(
-                {
-                    "val/roc_curve": wandb_plot.roc_curve(
-                        y_true=y_true[:10000],
-                        y_probas=y_probas[:10000],
-                        labels=self.class_names,
-                    )
-                }
-            )
-            wandb.log(
-                {
-                    "val/conf_mat": wandb_plot.confusion_matrix(
-                        probs=y_probas,
-                        y_true=y_true,
-                        class_names=self.class_names,
-                    )
-                }
-            )
-
-    def test_step(
-        self, *args, **kwargs
-    ) -> Tuple[Tensor, Tensor, Tensor]:  # pylint: disable=unused-argument
-        batch = args[0]
-        output = self(batch[:, :-1])
-        loss = self.loss(output, batch[:, 1:])
-        acc = self.acc(output, batch[:, 1:])
-        self.log("test/loss", loss)
-        self.log("test/acc", acc)
-        return loss, output, batch[:, 1:]
+        self.epoch_end_template(outputs, "val")
 
     def test_epoch_end(self, outputs: List[Tuple[Tensor, Tensor, Tensor]]) -> None:
-        _, output_list, target_list = zip(*outputs)
-        score = torch.cat(output_list, dim=0).permute([0, 2, 1]).flatten(0, 1)
-        target = torch.cat(target_list, dim=0).flatten()
-        y_true = self.simplify_class(target).detach().cpu().numpy()
-        y_probas = self.simplify_score(score).detach().cpu().numpy()
-        if self.logger is not None:
-            wandb.log(
-                {
-                    "test/pr_curve": wandb_plot.pr_curve(
-                        y_true=y_true[:10000],
-                        y_probas=y_probas[:10000],
-                        labels=self.class_names,
-                    )
-                }
-            )
-            wandb.log(
-                {
-                    "test/roc_curve": wandb_plot.roc_curve(
-                        y_true=y_true[:10000],
-                        y_probas=y_probas[:10000],
-                        labels=self.class_names,
-                    )
-                }
-            )
-            wandb.log(
-                {
-                    "test/conf_mat": wandb_plot.confusion_matrix(
-                        probs=y_probas,
-                        y_true=y_true,
-                        class_names=self.class_names,
-                    )
-                }
-            )
+        self.epoch_end_template(outputs, "test")
 
     def configure_optimizers(self):
         return torch.optim.AdamW(
